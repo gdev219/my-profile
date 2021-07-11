0fc91d31009a61ceaf13d834b69fd2e92731cd89
박혜성 <hyesung219@aipers.com>
2021-07-11 20:05:00 +0900

    Feat(firebase): firebase for nuxt added

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,22 @@ export default {
   head: {
     title: 'my-profile',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -44,12 +40,29 @@ export default {
     '@nuxtjs/emotion',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    // https://firebase.nuxtjs.org/guide/getting-started
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: 'AIzaSyBZ2MAT8yum7Dwhxh2UAR0lQzmlyheWrH0',
+          authDomain: 'gdev219-profile-fcb9b.firebaseapp.com',
+          databaseURL: 'https://gdev219-profile-fcb9b.firebaseio.com',
+          projectId: 'gdev219-profile-fcb9b',
+          storageBucket: 'gdev219-profile-fcb9b.appspot.com',
+          messagingSenderId: '14701232827',
+          appId: '1:14701232827:web:f1096e6fd3b56147b09f9e',
+        },
+        services: {
+          auth: true, // Just as example. Can be any other service.
+        },
+      },
+    ],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
